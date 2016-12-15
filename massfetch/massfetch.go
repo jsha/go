e16fae9b33f5517b0a6a6c8b9ac2704e2360defc
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-12-14 18:01:52 -0800

    Add massfetch.

---
+++ massfetch/massfetch.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+var n = flag.Int("n", 1, "Number of requests to make")
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Println("provide exactly one URL on command line.")
+	}
+	var wg sync.WaitGroup
+	for i := 0; i < *n; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			resp, err := http.Get(args[0])
+			if err != nil {
+				fmt.Printf("err: %s\n", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			fmt.Printf("HTTP %d\n", resp.StatusCode)
+			for k, vv := range resp.Header {
+				for _, v := range vv {
+					fmt.Printf("%s: %s\n", k, v)
+				}
+			}
+			body, err := ioutil.ReadAll(resp.Body)
+			fmt.Printf("\n%s\n", string(body))
+		}()
+	}
+	wg.Wait()
+}
