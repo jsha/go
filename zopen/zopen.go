58fd246a97f2174fb2f4456a7b8b614d103efc93
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-03-01 14:35:19 -0800

    Add OpenMany.

--- zopen/zopen.go
+++ zopen/zopen.go
@@ -3,10 +3,14 @@
 package zopen
 
 import (
+	"bufio"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
+	"runtime"
 	"strings"
+	"sync"
 )
 
 type File struct {
@@ -24,6 +28,9 @@ func (f *File) Close() error {
 	return f.ReadCloser.Close()
 }
 
+// Open acts exactly like os.Open when filename does not end in ".gz". When
+// filename ends in ".gz", Open  returns a File object that gives the
+// uncompressed contents of the file.
 func Open(filename string) (*File, error) {
 	var f io.ReadCloser
 	var err error
@@ -40,4 +47,52 @@ func Open(filename string) (*File, error) {
 	}
 
 	return &File{f, nil}, nil
+}
+
+// OpenMany will call Open on each of the filenames, and return a chan string
+// that produces lines from those files. Lines from different files may be
+// interleaved nondeterministically.
+func OpenMany(filenames []string) (<-chan string, error) {
+	// Don't open all the files at once or we'll get "too many open files"
+	// also it would be wasteful of memory.
+	concurrency := runtime.NumCPU()
+	sem := make(chan bool, concurrency)
+
+	// Results will be sent through here.
+	ch := make(chan string)
+	// This is how we know when all the goroutines are done.
+	var wg sync.WaitGroup
+
+	go func() {
+		for _, fn := range filenames {
+			sem <- true
+			handle, err := Open(fn)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "opening %q: %s", fn, err)
+			}
+			wg.Add(1)
+			go process(handle, ch, &wg, sem)
+		}
+
+		wg.Wait()
+		for i := 0; i < cap(sem); i++ {
+			sem <- true
+		}
+		close(ch)
+	}()
+
+	return ch, nil
+}
+
+func process(f *File, ch chan<- string, wg *sync.WaitGroup, sem <-chan bool) {
+	defer f.Close()
+	defer func() { <-sem }()
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		ch <- scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+	wg.Done()
 }
